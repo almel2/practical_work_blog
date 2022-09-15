8c124f8cba242584181fff6624fb663ecd960629
Aleksandr <Al.Melnikov2@gmail.com>
2022-09-15 14:39:10 +0300

    added views

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from blog import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('post-list/', views.PostList.as_view(), name='post_list'),
+    path('user-posts/', views.UserPostList.as_view(), name='user_posts'),
+    path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,39 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from blog.models import Post
+
+User = get_user_model()
 
 
 def index(request):
     return render(request, 'blog/index.html', context={})
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.all()
+
+
+class PostDetail(DetailView):
+    model = Post
+    queryset = Post.objects.all()
+    pk_url_kwarg = 'pk'
+    template_name = 'blog/post_detaile.html'
+
+
+class UserPostList(ListView):
+    model = User
+    queryset = User.objects.all()
+    template_name = 'blog/user_posts_list.html'
+    context_object_name = 'users'
+
+
+class ProfilePublic(DetailView):
+    model = User
+    queryset = User.objects.all()
+    context_object_name = 'users'
+    pk_url_kwarg = 'pk'
+    template_name = 'blog/profile_public.html'
