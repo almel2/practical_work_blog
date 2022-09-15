f843ff31784405586e1d7efbe2c5e16eb5278da1
Aleksandr <Al.Melnikov2@gmail.com>
2022-09-15 12:24:57 +0300

    added models

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import login
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 from accounts.forms import CastomUserForm
+
 
 class RegistrationUser(FormView):
     form_class = CastomUserForm
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, default='Anonimus')
+    comment = models.TextField()
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE)
+    is_published = models.BooleanField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment[:10]
